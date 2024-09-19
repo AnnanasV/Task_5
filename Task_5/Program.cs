c828bb8f89dd8ceba8d441a82583c972c35a5d22
Анна <vindanka@gmail.com>
2024-09-20 00:32:52 +0300

    create thermostat and check if it works

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿Thermostat.Thermostat thermostat = new Thermostat.Thermostat();
+thermostat.ChangeTemperature(15);
+thermostat.ChangeTemperature(17);
---
+++ Thermostat/Thermostat.cs
@@ -0,0 +1,30 @@
+﻿namespace Thermostat
+{
+    public class Thermostat
+    {
+        private int temperature;
+        delegate void TemperatureChangedEventHandler(int temperature);
+        event TemperatureChangedEventHandler TemperatureChanged;
+
+        public Thermostat()
+        {
+            TemperatureChanged += ShowTemperature;
+        }
+
+        private void OnTemperatureChanged(int temperature)
+        {
+            TemperatureChanged?.Invoke(temperature);
+        }
+
+        public void ChangeTemperature(int temperature)
+        {
+            this.temperature = temperature;
+            OnTemperatureChanged(temperature);
+        }
+
+        public void ShowTemperature(int temperature)
+        {
+            Console.WriteLine("Temperature is " + temperature);
+        }
+    }
+}
