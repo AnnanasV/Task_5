1994164d3b530d13748ad31e00360d355f43f89f
Анна <vindanka@gmail.com>
2024-09-20 02:44:57 +0300

    create sorting class for even numbers and numbers more than given number, check if it is working

---
+++ Sorting/Sorting.cs
@@ -0,0 +1,22 @@
+﻿namespace Sorting
+{
+    public class Sorting
+    {
+        public static Action<int[]> ShowEvenNumbers = (int[] numbers) =>
+        {
+            Console.WriteLine("Even numbers:");
+            foreach (var n in numbers)
+            {
+                if(n % 2 == 0) Console.WriteLine(n);
+            }
+        };
+
+        public static Action<int[], int> ShowMoreNumbers = (int[] numbers, int number) =>
+        {
+            foreach (var n in numbers)
+            {
+                if(n > number) Console.WriteLine(n + " is more than " + number);
+            }
+        };
+    }
+}
--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,3 +1,7 @@
 ﻿Thermostat.Thermostat thermostat = new Thermostat.Thermostat();
 thermostat.ChangeTemperature(15);
 thermostat.ChangeTemperature(17);
+
+var numbers = new int[] { 1, 4, 2, 6, 7 };
+Sorting.Sorting.ShowEvenNumbers(numbers);
+Sorting.Sorting.ShowMoreNumbers(numbers, 5);
