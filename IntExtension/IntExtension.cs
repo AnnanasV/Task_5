348900e50160dcc34b2d31fc62252886bb340ac0
Анна <vindanka@gmail.com>
2024-09-20 03:10:37 +0300

    create extension method to find average number and test it

---
+++ IntExtension/IntExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace IntExtension
+{
+    public static class IntExtension
+    {
+        public static double Average(this int[] numbers)
+        {
+            double averageNum = 0;
+            foreach (int i in numbers)
+            {
+                averageNum += i;
+            }
+            return averageNum / numbers.Length;
+        }
+    }
+}
--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,7 +1,12 @@
-﻿Thermostat.Thermostat thermostat = new Thermostat.Thermostat();
+﻿using static IntExtension.IntExtension;
+Thermostat.Thermostat thermostat = new Thermostat.Thermostat();
 thermostat.ChangeTemperature(15);
 thermostat.ChangeTemperature(17);
 
-var numbers = new int[] { 1, 4, 2, 6, 7 };
-Sorting.Sorting.ShowEvenNumbers(numbers);
-Sorting.Sorting.ShowMoreNumbers(numbers, 5);
+var numbers1 = new int[] { 1, 4, 2, 6, 7, 7 };
+var numbers2 = new int[] { 1, 2, 3, 4, 5 };
+Sorting.Sorting.ShowEvenNumbers(numbers1);
+Sorting.Sorting.ShowMoreNumbers(numbers1, 5);
+
+Console.WriteLine("Average is " + numbers1.Average());
+Console.WriteLine("Average is " + numbers2.Average());
